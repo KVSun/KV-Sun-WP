2b05358adfbb590c83ee311870b0a4c97e90f3c6
Chris Zuber <shgysk8zer0@gmail.com>
2016-09-14 13:42:16 -0700

    Cleanup and set debigging const
    Combine consecutive PHP blocks
    Define `DEBUG_MODE`
    Set custom error and exception handlers based on `DEBUG_MODE`

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
-	ob_start();
-	set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'classes' . PATH_SEPARATOR . get_include_path());
-	spl_autoload_register('spl_autoload');
-	$console = \shgysk8zer0\Core\Console::getInstance();
-	$console->asErrorHandler();
-	$console->asExceptionHandler();
-	$timer = new \shgysk8zer0\Core\Timer();
+	define('DEBUG_MODE', true);
+	if (DEBUG_MODE) {
+		ob_start();
+		set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'classes' . PATH_SEPARATOR . get_include_path());
+		spl_autoload_register('spl_autoload');
+		$console = \shgysk8zer0\Core\Console::getInstance();
+		$console->asErrorHandler();
+		$console->asExceptionHandler();
+		$timer = new \shgysk8zer0\Core\Timer();
+	}
+	get_header();
 ?>
-<?php get_header();  ?>
 <!--#blocks-wrapper-->
 <div class="blocks-wrapper clearfix">
 <!--#blocks-left-or-right-->
@@ -27,10 +30,11 @@
 
   			</div>
  			<!-- /blocks col -->
- <?php get_sidebar();  ?>
-
- <?php get_footer(); ?>
  <?php
-	$console->log("Loaded in {$timer} ms.");
-	$console->sendLogHeader();
+ 	get_sidebar();
+	get_footer();
+	if (DEBUG_MODE) {
+		$console->log("Loaded in {$timer} ms.");
+		$console->sendLogHeader();
+	}
 ?>
