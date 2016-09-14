fc68f0dea9916987f68d3f4a56247a03f494fade
Chris Zuber <shgysk8zer0@gmail.com>
2016-09-14 14:14:02 -0700

    Re-fix sticky positioning

--- header.php
+++ header.php
@@ -8,30 +8,30 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <base href="/"/>
 	<meta charset="utf-8" />
-	
+
 	<meta name="referrer" content="origin-when-cross-origin"/>
 	<meta name="viewport" content="width=device-width, height=device-height" />
 	<meta name="mobile-web-app-capable" content="yes" />
 	<meta name="theme-color" content="#EA5708" />
-	<?php if(is_single()){ ?>		
-		<meta itemprop="name" content="<?php the_title(); ?>" />		
+	<?php if(is_single()){ ?>
+		<meta itemprop="name" content="<?php the_title(); ?>" />
 		<meta itemprop="url" name="url" content="<?php echo $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>" />
-		
+
 	<?php }else{ ?>
-		<meta itemprop="name" content="KVSun-WP-Theme" />	
-	<?php } ?>			
+		<meta itemprop="name" content="KVSun-WP-Theme" />
+	<?php } ?>
 	<meta property="fb:app_id" content="" />
 	<meta name="og:title" content="KVSun-WP-Theme" />
 	<meta name="og:description" content="A custom theme for the Kern Valley Sun Wordpress site." />
-	<meta name="og:image" content="<?php echo get_template_directory_uri(); ?>/images/sun-icons/32.png" />		
+	<meta name="og:image" content="<?php echo get_template_directory_uri(); ?>/images/sun-icons/32.png" />
 	<meta name="twitter:site" content="@kvsun" />
 	<meta name="twitter:title" content="KVSun-WP-Theme" />
 	<meta name="twitter:description" content="A custom theme for the Kern Valley Sun Wordpress site." />
 	<meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/sun-icons/32.png" />
-	<meta name="twitter:card" content="summary_large_image" />			
-	<meta name="robots" content="index,noarchive">	
-	
-	
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="robots" content="index,noarchive">
+
+
 		<link rel="apple-touch-icon" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/images/sun-icons/16.png" />
 	<link rel="icon" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/images/sun-icons/16.png" type="image/png" />
 		<link rel="apple-touch-icon" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/images/sun-icons/32.png" />
@@ -42,7 +42,7 @@
 	<link rel="icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/images/sun-icons/144.png" type="image/png" />
 		<link rel="apple-touch-icon" sizes="256x256" href="<?php echo get_template_directory_uri(); ?>/images/sun-icons/256.png" />
 	<link rel="icon" sizes="256x256" href="<?php echo get_template_directory_uri(); ?>/images/sun-icons/256.png" type="image/png" />
-	
+
 	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/manifest.json" />
 
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
@@ -53,7 +53,7 @@
 <?php global $bresponZive_tpcrn_data;?>
 <title>
 
-<?php   if (is_home()) { 
+<?php   if (is_home()) {
    wp_title( '|', true, 'right' );
    }
    else { the_title();} ?>
@@ -70,7 +70,7 @@
 
 <?php wp_head();?>
 
- 
+
 </head>
 
 <body <?php body_class();?>>
@@ -84,16 +84,16 @@
 
 <div id="">
 <div id="header">
-  <div id="head-content" class="clearfix "> 
-    
+  <div id="head-content" class="clearfix ">
+
     <!-- Logo -->
-    
+
     <div id="logo">
-      <?php if($bresponZive_tpcrn_data['custom_logo'] !='') { 
+      <?php if($bresponZive_tpcrn_data['custom_logo'] !='') {
 
-				if($bresponZive_tpcrn_data['custom_logo']) {  $logo = $bresponZive_tpcrn_data['custom_logo']; 		
+				if($bresponZive_tpcrn_data['custom_logo']) {  $logo = $bresponZive_tpcrn_data['custom_logo'];
 
-				} else { $logo = get_template_directory_uri() . '/images/logo.png'; 	
+				} else { $logo = get_template_directory_uri() . '/images/logo.png';
 
 				} ?>
       <a href="<?php echo esc_url( home_url( '/' ) );  ?>" title="<?php bloginfo( 'name' ); ?>" rel="home"><img  src="<?php echo esc_url($logo); ?>" alt="<?php bloginfo( 'name' ) ?>" /></a>
@@ -111,16 +111,16 @@
         </a></h2>
       <?php } } ?>
     </div>
-    <?php 
+    <?php
 
-     if ( has_nav_menu('topNav') ){ 
+     if ( has_nav_menu('topNav') ){
 
    ?>
 
 <!-- #CatNav -->
 
 <div id="btn-nav" class="top-right-nav mobile-nav">
-  <?php 
+  <?php
 		if ( is_user_logged_in() ) {
 			wp_nav_menu(array( 'menu' => 'Restrict_menu','theme_location' => 'restrictMenu','container'=> '','menu_id'=> '','menu_class'=> 'container clearfix','fallback_cb' => 'false','depth' => 3));
 		} else {
@@ -130,7 +130,7 @@
 </div>
 <!-- /#CatNav --->
 <?php } ?>
-    <!-- /#Logo -->     
+    <!-- /#Logo -->
   </div>
 </div>
 <!-- /#Header -->
@@ -138,8 +138,8 @@
 </div>
 
 <?php if (is_home()) { ?>
-<div id="catnav" class="secondary mobile-nav container-fluid menu-part" itemscope itemtype="http://schema.org/SiteNavigationElement">
-  <?php wp_nav_menu(array('theme_location' => 'mainNav','container'=> '','menu_id'=> 'catmenu','menu_class'=> 'catnav clearfix','fallback_cb' => 'false','depth' => 3)); 
+<div id="catnav" class="secondary sticky mobile-nav container-fluid menu-part" itemscope itemtype="http://schema.org/SiteNavigationElement">
+  <?php wp_nav_menu(array('theme_location' => 'mainNav','container'=> '','menu_id'=> 'catmenu','menu_class'=> 'catnav clearfix','fallback_cb' => 'false','depth' => 3));
 		?>
 </div>
 <!-- Slider start ---->
@@ -154,7 +154,7 @@
 	 <?php } else { if ( has_nav_menu('mainNav') ){  ?>
 <!-- #CatNav -->
 <div id="catnav" class="secondary mobile-nav container-fluid">
-  <?php wp_nav_menu(array('theme_location' => 'mainNav','container'=> '','menu_id'=> 'catmenu','menu_class'=> 'catnav clearfix','fallback_cb' => 'false','depth' => 3)); 
+  <?php wp_nav_menu(array('theme_location' => 'mainNav','container'=> '','menu_id'=> 'catmenu','menu_class'=> 'catnav clearfix','fallback_cb' => 'false','depth' => 3));
 		?>
 <!-- /#CatNav -->
 </div>
@@ -163,5 +163,3 @@
 
 <div class="container-fluid clearfix">
 <div>
-
-
--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,15 @@
-  $(document).ready(function(){
-
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 1){
-			$('.secondary').addClass("sticky");
-		  }
-		  else{
-			$('.secondary').removeClass("sticky");
-		  }
-		});
- });
+if ( !(('CSS' in window) && ('supports' in CSS) && CSS.supports('position', 'sticky'))) {
+    $(document).ready(function(){
+       $(window).scroll(function() {
+           if ($(this).scrollTop() > 1){
+               $('.secondary.sticky').addClass("stuck");
+             }
+             else{
+               $('.secondary.sticky').removeClass("stuck");
+             }
+           });
+    });
+}
 
  function logoutConfirm(){
 	var x = confirm("Are you sure you want to log out?");
