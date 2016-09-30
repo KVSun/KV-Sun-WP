53aeaf10d98e8761cb272fdafbbfd21d2aea1287
Chris Zuber <shgysk8zer0@gmail.com>
2016-09-30 00:57:21 -0700

    Fix setting DEBUG_MODE
    Create function to check if administrator and define DEBUG_MODE using
    that.

--- autoloader.php
+++ autoloader.php
@@ -2,8 +2,11 @@
 namespace KVS;
 set_include_path(__DIR__ . DIRECTORY_SEPARATOR . 'classes' . PATH_SEPARATOR . get_include_path());
 spl_autoload_register('spl_autoload');
+define('DEBUG_MODE', is_admin());
+function is_admin()
+{
+	return function_exists('is_user_logged_in') and is_user_logged_in()
+		and in_array('administrator', wp_get_current_user()->roles);
+}
 
-function_exists('is_user_logged_in') and is_user_logged_in()
-and in_array('administrator', wp_get_current_user()->roles)
-	? define('DEBUG_MODE', true) : define('DEBUG_MODE', false);
 
