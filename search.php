52c2482031af42386bb4b8570c4dfcbae280146c
Chris Zuber <shgysk8zer0@gmail.com>
2016-09-30 14:57:11 -0700

    More cleanup and logging

--- search.php
+++ search.php
@@ -1,1 +1,28 @@
-<?php get_header(); ?><div id="blocks-wrapper" class="clearfix">	<div id="blocks-left" class="eleven columns clearfix">	 		 						<!--Search Results content--> <h2 class="blogpost-wrapper-title"> 							<?php printf(__('Search results for "%s" ', 'bresponZive'), get_search_query()); ?>  						</h2>  		      				<?php   get_template_part( 'includes/blog', 'loop' );?>		  			 	<!--/.blogposts-wrapper-->   </div>     <?php get_sidebar();?> <?php get_footer(); ?>
+<?php
+	namespace KVS;
+	require_once __DIR__ . DIRECTORY_SEPARATOR . 'autoloader.php';
+	if (DEBUG_MODE) {
+		ob_start();
+		$console = \shgysk8zer0\Core\Console::getInstance();
+		$timer = new \shgysk8zer0\Core\Timer();
+		$console->asErrorHandler();
+		$console->asExceptionHandler();
+	}
+	get_header();
+?>
+<div id="blocks-wrapper" class="clearfix">
+	<div id="blocks-left" class="eleven columns clearfix">
+		<!--Search Results content-->
+		<h2 class="blogpost-wrapper-title">
+			<?php printf(__('Search results for "%s" ', 'bresponZive'), get_search_query()); ?>
+		</h2>
+		<?php   get_template_part( 'includes/blog', 'loop' );?>
+		<!--/.blogposts-wrapper-->
+	</div>
+<?php
+	if (DEBUG_MODE) {
+		$console->log("Loaded in {$timer} ms.");
+		$console->info(get_included_files());
+		$console->sendLogHeader();
+	}
+?>
